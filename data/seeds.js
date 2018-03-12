586f9dddf55300fffb162bc14c30709aa8dec83a
Daniel Gruenthal <d.gruenthal@gmail.com>
2018-03-12 15:27:42 +0100

    Add seed data

---
+++ data/seeds.js
@@ -0,0 +1,14 @@
+import { v4 } from 'uuid';
+
+export default [
+  {
+    id: v4(),
+    description: 'buy some milk',
+    complete: false
+  },
+  {
+    id: v4(),
+    description: 'write notes for shop talk',
+    complete: true
+  }
+];
